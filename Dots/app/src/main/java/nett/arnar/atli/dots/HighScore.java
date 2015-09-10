d8e1e8faa25a369f1687938fabfa4403432ce40d
Atli <atligud13@ru.is>
2015-09-10 16:50:10 +0000

    Added the show high scores view, does not work at the moment

---
+++ Dots/app/src/main/java/nett/arnar/atli/dots/HighScore.java
@@ -0,0 +1,14 @@
+package nett.arnar.atli.dots;
+
+/**
+ * Created by Atli Guðlaugsson on 9/10/2015.
+ */
+public class HighScore {
+    public String name;
+    public String score;
+
+    public HighScore(String name, String score) {
+        this.name = name;
+        this.score = score;
+    }
+}
