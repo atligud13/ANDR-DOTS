2441cce9d0b05f36a528a0bd722992ef9afe92be
Arnar Gauti Ingason <granra@gmail.com>
2015-09-17 18:18:29 +0000

    Animations finished

--- Dots/app/src/main/java/nett/arnar/atli/dots/Shapes/Circle.java
+++ Dots/app/src/main/java/nett/arnar/atli/dots/Shapes/Circle.java
@@ -1,28 +1,71 @@
 package nett.arnar.atli.dots.Shapes;
 
+import android.animation.ValueAnimator;
+import android.animation.ValueAnimator.AnimatorUpdateListener;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
 
+import nett.arnar.atli.dots.BoardView;
+
 /**
  * Created by arnar on 9/10/15.
  */
 public class Circle {
-    RectF circle;
-    int color;
-    int radius;
+    private RectF circle;
+    private int color;
+    private int radius;
+    private BoardView view;
+    private ValueAnimator locationAnimator = new ValueAnimator();
+    private ValueAnimator radiusAnimator = new ValueAnimator();
 
-    public Circle(int color, int radius) {
+    private AnimatorUpdateListener locationListener = new AnimatorUpdateListener() {
+        @Override
+        public void onAnimationUpdate(ValueAnimator animation) {
+            int newY = (int) animation.getAnimatedValue();
+            moveTo((int) getX(), newY);
+            view.invalidate();
+        }
+    };
+
+    private AnimatorUpdateListener radiusListener = new AnimatorUpdateListener() {
+        @Override
+        public void onAnimationUpdate(ValueAnimator animation) {
+            int newRadius = (int) animation.getAnimatedValue();
+            setRadius(newRadius);
+            view.invalidate();
+        }
+    };
+
+    public Circle(BoardView view, int color, int radius, int x, int y) {
         this.circle = new RectF();
+        this.circle.set(x - radius, y - radius, x + radius, y + radius);
         this.color = color;
         this.radius = radius;
+        this.view = view;
+
+        this.radiusAnimator.setDuration(200);
+        this.radiusAnimator.setIntValues(1, radius);
+        this.radiusAnimator.addUpdateListener(radiusListener);
+        this.radiusAnimator.start();
     }
 
-    public void draw(Canvas canvas, Paint paint, int x, int y) {
-        circle.set(x - radius, y - radius, x + radius, y + radius);
+    public void draw(Canvas canvas, Paint paint) {
         paint.setColor(color);
         canvas.drawOval(circle, paint);
+    }
+
+    public void moveTo(int x, int y) {
+        this.circle.set(x - radius, y - radius, x + radius, y + radius);
+    }
+
+    public void animateY(int yTo) {
+        locationAnimator.removeAllUpdateListeners();
+        locationAnimator.setDuration(200);
+        locationAnimator.setIntValues((int)getY(), yTo);
+        locationAnimator.addUpdateListener(locationListener);
+        locationAnimator.start();
     }
 
     public RectF getCircle() {
@@ -39,5 +82,12 @@ public class Circle {
 
     public int getColor() {
         return color;
+    }
+
+    private void setRadius(int rad) {
+        int x = (int)getX();
+        int y = (int)getY();
+        this.radius = rad;
+        circle.set(x - rad, y - rad, x + rad, y + rad);
     }
 }
